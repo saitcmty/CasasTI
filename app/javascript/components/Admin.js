ca14d3f3e3f800c94859e989f2770ffc318c9354
albgv1402 <albertogarv1402@gmail.com>
2020-11-05 23:02:55 -0600

    Admin panel finished

---
+++ app/javascript/components/Admin.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import AdminButton from './Admin/AdminButton';
+
+class Admin extends Component {
+    render() {
+        return (
+            <div>
+               <h1 className="main-title">BIENVENIDO {this.props.userName}!! TOP SECRET</h1>
+
+                <section id="admin-actions" className="row">
+                    <AdminButton 
+                        buttonID="houses"
+                        buttonRedirect="/houses"
+                        buttonText="CASAS"
+                    />
+
+                    <AdminButton 
+                        buttonID="students"
+                        buttonRedirect="/students"
+                        buttonText="ESTUDIANTES"
+                    />
+
+                    <AdminButton 
+                        buttonID="events"
+                        buttonRedirect="/houses"
+                        buttonText="EVENTOS"
+                    />
+
+                    <AdminButton 
+                        buttonID="registrations"
+                        buttonRedirect="/houses"
+                        buttonText="REGISTROS"
+                    />
+
+                    <AdminButton 
+                        buttonID="evidences"
+                        buttonRedirect="/houses"
+                        buttonText="EVIDENCIAS"
+                    />
+
+                    <AdminButton 
+                        buttonID="redirects"
+                        buttonRedirect="/houses"
+                        buttonText="REDIRECTS"
+                    />
+
+                </section> 
+            </div>
+        );
+    }
+}
+
+Admin.propTypes = {
+    userName: PropTypes.string.isRequired
+};
+
+export default Admin;
