f242abe2cbb291c796b0df8e07cb97a560691b29
albgv1402 <albertogarv1402@gmail.com>
2020-11-17 18:55:46 -0600

    Student List filtering correctly according to house

--- app/javascript/components/StudentsAdmin.js
+++ app/javascript/components/StudentsAdmin.js
@@ -3,17 +3,67 @@ import PropTypes from 'prop-types';
 import StudentList from './Admin/Students/StudentList';
 
 class StudentsAdmin extends Component {
+    state = {
+        studentList: [],
+        filters: {
+            houses: {
+                Cuervos: true,
+                Gallinas: true,
+                Patos: true,
+                Pavorreales: true,
+                Venados: true,
+            }
+        }
+    }
+
+    filterList() {
+        let list = this.props.students;
+        let keys = Object.keys(this.state.filters.houses);
+
+        keys.forEach(house => {
+            if (!this.state.filters.houses[house]) {
+                list = list.filter(student => student.house_id != house);
+            }
+        })
+
+        this.setState({studentList: list});
+    }
+
+    changeHouseFilters(house) {
+        console.log(house);
+        let newHouses = this.state.filters.houses
+        newHouses[house] = !newHouses[house];
+        this.setState({filters: {houses: newHouses}});
+        this.filterList();
+    }
+
+    componentDidMount() {
+        this.setState({studentList: this.props.students})
+    }
+
     render() {
+        console.log(this.props.students)
         return (
-            <div className="row">
-                <section id="all-students-panel" className="panel-component">
-                <p className="label">ESTUDIANTES:</p>
-                <div id="students-container" className="elements-container">
-                    <StudentList
-                        students={this.props.students}
-                    />
+            <div>
+                <div className="row admin-student-list-filters">
+                    <div id="houses-filter">
+                        <img onClick={this.changeHouseFilters.bind(this, 'Cuervos')} className="house-filter-img" src="/casas/cuervos.png"/>
+                        <img onClick={this.changeHouseFilters.bind(this, 'Gallinas')} className="house-filter-img" src="/casas/gallinas.png"/>
+                        <img onClick={this.changeHouseFilters.bind(this, 'Patos')} className="house-filter-img" src="/casas/patos.png"/>
+                        <img onClick={this.changeHouseFilters.bind(this, 'Pavorreales')} className="house-filter-img" src="/casas/pavo_reales.png"/>
+                        <img onClick={this.changeHouseFilters.bind(this, 'Venados')} className="house-filter-img" src="/casas/venados.png"/>
+                    </div>
                 </div>
-                </section>
+                <div className="row">
+                    <section id="all-students-panel" className="panel-component">
+                    <p className="label">ESTUDIANTES:</p>
+                    <div id="students-container" className="elements-container">
+                        <StudentList
+                            students={this.state.studentList}
+                        />
+                    </div>
+                    </section>
+                </div>
             </div>
         );
     }
