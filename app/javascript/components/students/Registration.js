9e636c5f538398aca5b670cf017f60f57f2080f5
albgv1402 <albertogarv1402@gmail.com>
2020-11-25 00:52:53 -0600

    Formatted date of evidences of Students and each evidence show its points

--- app/javascript/components/students/Registration.js
+++ app/javascript/components/students/Registration.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import moment from "moment";
 import PropTypes from 'prop-types';
 
 class Registration extends Component {
+    state = {
+        date: ''
+    }  
+
+    componentDidMount() {
+        console.log(this.props);
+        const dateString = moment(this.props.registration.date).locale("es").format("DD-MMM-YYYY hh:mma");
+
+        this.setState({date: dateString});
+    }
+
     render() {
         return (
             <div>
                 <div className="registration-component element-component">
                     <div className="info">
                         <p className="title">{this.props.evidence.title}</p>
-                        <p className="date">{this.props.registration.date}</p>
+                        <p className="date">{this.state.date}</p>
                     </div>
+                    <p className="student-points-panel">Puntos: {this.props.evidence.points}</p>
                     <div className="icon-container">
                         <Approved approved={this.props.registration.approved}/>
                     </div>
